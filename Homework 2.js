73617367c87364b0b67b65782d7a4a328ddfb65b
Levon <92533943+Kost333@users.noreply.github.com>
2022-02-25 18:51:01 +0300

    Add files via upload

---
+++ Homework 2.js
@@ -0,0 +1,107 @@
+// 1.Declare 2 variables a and b,
+// such that a > b,
+// and define values for them. Output their
+
+// let a = 10;
+// let b = 5;
+//
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// 2.Print your name and age in the following format:
+// “My name is ____, I am ____ .”
+
+// let name = prompt("My name is ");
+// let age = prompt(" I am ");
+// alert("My name is " + name + "," + " I am " + age);
+
+// 3.Declare a variable with defined value.
+// Print the last digit of the number.
+
+// let number = prompt("Number");
+// let lastDigit = number % 10;
+// alert("The last digit of " + number + " is " + lastDigit);
+
+// 4.Check whether a given number is negative.
+// Print “yes”, if it is negative, print “no”
+// otherwise.
+
+// let number = prompt("Number")
+// if(number < 0) {
+//     alert("yes")
+// }else {
+//     alert("no")
+// }
+
+// 5.Given two numbers print 1 if one of them is divisible
+// by the other one, otherwise print 0.
+
+// let num1 = prompt("Write number 1");
+// let num2 = prompt("Write number 2");
+// if(num1 % num2 === 0 || num2 % num1 === 0) {
+//     alert("1")
+// } else {
+//     alert("0")
+// }
+
+// 6.Given any number between 1 and 12.
+// Print the name of the respective month.
+
+// let MonthsNumber = prompt("Write month number");
+// switch (MonthsNumber) {
+//     case "1":
+//         alert("January");
+//         break;
+//     case "2":
+//         alert("February");
+//         break;
+//     case "3":
+//         alert("March");
+//         break;
+//     case "4":
+//         alert("April");
+//         break;
+//     case "5":
+//         alert("May");
+//         break;
+//     case "6":
+//         alert("June");
+//         break;
+//     case "7":
+//         alert("July");
+//         break;
+//     case "8":
+//         alert("August");
+//         break;
+//     case "9":
+//         alert("September");
+//         break;
+//     case "10":
+//         alert("October");
+//         break;
+//     case "11":
+//         alert("November");
+//         break;
+//     case "12":
+//         alert("December");
+//         break;
+// }
+
+// 7.Given three numbers. Find the maximum one.
+
+// let num1 = prompt("Write number 1");
+// let num2 = prompt("Write number 2");
+// let num3 = prompt("Write number 3");
+//
+// if(num1 > num2 && num1 > num3){
+//     alert(num1)
+// }else if(num2 > num1 && num2 > num3){
+//     alert(num2)
+// }else if(num3 > num1 && num3 > num2){
+//     alert(num3)
+// }else {
+//     alert("equal")
+// }
