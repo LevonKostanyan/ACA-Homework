d4ac924f951283e9bb63e86fd21b1a4f9a7752ae
Levon <92533943+Kost333@users.noreply.github.com>
2022-03-27 20:47:36 +0400

    Update Homework 5.js

--- Homework 5.js
+++ Homework 5.js
@@ -37,3 +37,58 @@ function checkPalindrome(string){
 }
 console.log(checkPalindrome(""))
 
+// 4. Given an array of numbers. Find the maximum element in array.
+
+function numbersArr(arr) {
+
+// arr = arr.sort((a,b) => a - b)
+// return arr[arr.length - 1]
+
+// or
+
+let maxValue = 0
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxValue) {
+        maxValue = arr[i]
+    }
+}
+return maxValue
+}
+
+console.log(numbersArr([1, 5, 8, 12, 45, 2, -99, 564, 6546, 6313, -56151, -5615]))
+
+// 5. Given an array. Create the array which elements are products between two neighbours.
+
+function prodBetTwoNeigh(arr) {
+    let result = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        result.push(arr[i] * arr[i + 1])
+    }
+    return result
+}
+
+console.log(prodBetTwoNeigh([3, 7, 12, 5, 20, 0]))
+
+// 6. Given a string and symbols. Change first symbol by the second one in the string.
+
+function foo(str, a, b) {
+    console.log(str.split(a).join(b))
+}
+
+foo("does the following code", "o", "0")
+
+// 7. Print the following number pattern:
+
+function strNum(n) {
+    let str = ""
+    for (let i = 1; i <= n; i++) {
+        str += i
+        console.log(str)
+    }
+    for (let i = n - 1; i >= 1; i--) {
+        str = str.slice(0, i)
+        console.log(str)
+    }
+}
+
+strNum(5)
